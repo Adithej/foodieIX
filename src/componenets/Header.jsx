ed0e8da651dff045404ddfadafa0b736f6ad1f43
Adithya Thejas S <adithyathejass.1997@gmail.com>
2024-03-03 16:01:02 +0530

    responsiveness

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -6,36 +6,76 @@ import themeLight from "../assets/night.png";
 import themeDark from "../assets/day.png";
 
 const Header = ({ theme, setTheme }) => {
+  const hideSidebar = () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+  };
+
+  const showSidebar = () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+  };
+
   const toggleMode = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
   return (
-    <div className="header">
-      <div className="logo">
-        <img src="../pizza.svg" alt="logo" />
-        <h1>FoodieIX</h1>
-      </div>
-
-      <ul>
+    <nav>
+      <ul onClick={hideSidebar} className="sidebar">
+        <li>
+          <a href="#"></a>
+        </li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+        </svg>
         <li>Home</li>
-        <li>Products</li>
         <li>Features</li>
+        <li>Products</li>
         <li>About</li>
       </ul>
-      <div className="search-box">
-        <input type="text" placeholder="search here" />
+      <div className="header">
+        <div className="logo">
+          <img src="../pizza.svg" alt="logo" />
+          <h1>FoodieIX</h1>
+        </div>
+
+        <ul className="hideOnMobile">
+          <li>Home</li>
+          <li>Products</li>
+          <li>Features</li>
+          <li>About</li>
+        </ul>
+        <div className="search-box">
+          <input type="text" placeholder="search here" />
+          <img
+            src={theme === "light" ? searchIconDay : searchIconNight}
+            alt="search icon"
+          />
+        </div>
         <img
-          src={theme === "light" ? searchIconDay : searchIconNight}
-          alt="search icon"
+          onClick={toggleMode}
+          src={theme === "light" ? themeLight : themeDark}
+          alt="theme"
+          className="toggle-icon hideOnMobile"
         />
+        <div className="menu-button" onClick={showSidebar}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+            fill="#B2BEB5"
+          >
+            <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+          </svg>
+        </div>
       </div>
-      <img
-        onClick={toggleMode}
-        src={theme === "light" ? themeLight : themeDark}
-        alt="theme"
-        className="toggle-icon"
-      />
-    </div>
+    </nav>
   );
 };
 
